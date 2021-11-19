1a9b49af9d570def5e205494e74590fbb1edf349
Kent Nagata <ngtknt@me.com>
2021-11-19 16:41:31 +0900

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ReactElement } from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 type Props = {
-  styleTags: any;
+  styleTags: ReactElement<{}>;
 };
 
 export default class MyDocument extends Document<Props> {
-  static getInitialProps({renderPage}) {
+  static getServerSideProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-
-    const page = renderPage(App => props =>
+    const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />),
     );
-
     const styleTags = sheet.getStyleElement();
 
-    return {...page, styleTags};
+    return { ...page, styleTags };
   }
 
   render() {
     return (
-      <Html lang="ja">
+      <html>
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet" />
           {this.props.styleTags}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     );
   }
 }
